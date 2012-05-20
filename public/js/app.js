d8caeec6df0f62e60254622e29b394aa008fef8f
Darren_N <info@v25media.com>
2012-05-19 20:56:43 -0400

    Comment and delete functions in place

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-// Tell Underscore.js to use Mustache style templates
+// Tell Underscore.js to use {{ Mustache }} style templates
 _.templateSettings = {
   interpolate : /\{\{(.+?)\}\}/g
 };
@@ -19,7 +19,9 @@ Statigram.Photo = Backbone.Model.extend({
 
 // Collection - stores a group of Models
 Statigram.Photos = Backbone.Collection.extend({
+	url : '/photo',
 	model      : Statigram.Photo,
+
 	initialize : function() {
 		this.view = new Statigram.PhotoSheetView();
 		this.view.collection = this; // Give view a reference to this collection
@@ -27,20 +29,71 @@ Statigram.Photos = Backbone.Collection.extend({
 	}
 });
 
-// View - individual Photos
+// View - individual Photos. Magick happens here.
 Statigram.PhotoView = Backbone.View.extend({
 	tagName    : 'li',
 	className  : 'photo',
+
 	initialize : function() {
 		this.template = _.template($('#tpl_photo').html());
 	},
+
 	render : function() {
 		var data = this.model.attributes;
-		// Null vals show up in form so we need to manually nuke
+		// Null vals show up in form so we need to manually nuke. *boom*
 		if (data.comment === null) {
 			data.comment = '';
 		}
 		this.$el.append(this.template(this.model.attributes));
+	},
+
+	events : {
+		"click .button-comment" : "toggle_form",
+		"click .button-delete"  : "delete_photo",
+		"click input[type=submit]" : "submit_comment"
+	},
+
+	toggle_form : function(e) {
+		e.preventDefault();
+		if (this.options.form_state !== 1) {
+			this.options.form_state = 1;
+			this.$('.photo-form').fadeIn('fast');
+			this.$('.button-comment').css({ color : "#333" });
+		} else {
+			this.options.form_state = 0;
+			this.$('.photo-form').fadeOut('fast');
+			this.$('.button-comment').css({ color : "#999" });
+		}
+	},
+
+	delete_photo : function(e) {
+		e.preventDefault();
+		this.model.destroy({
+			success : function(model, response) {
+				model.view.$el.fadeOut('fast');
+			},
+			error : function(model, response) {
+				model.view.$('h1').css({
+					color : "#f00"
+				});
+			}
+		});
+	},
+
+	submit_comment : function(e) {
+		e.preventDefault();
+		this.model.save({comment : this.$('textarea').val()},{
+			success : function(model, response) {
+				model.view.$('label').
+				css({ color : 'green' }).
+				html('Comment saved!');
+			},
+			error : function(model, response) {
+				model.view.$('label').
+				css({ color : 'red' }).
+				html('Could not save commemnt');
+			}
+		});
 	}
 });
 
@@ -50,6 +103,7 @@ Statigram.PhotoSheetView = Backbone.View.extend({
 	initialize : function () {
 		this.container = $('#photos');
 	},
+
 	// Assemble PhotoViews from Photos reference and insert into container.
 	// We stash all of the PhotoViews into PhotoSheetView.$el (ul) and then
 	// insert $el into container (#photos)
